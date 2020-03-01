ef70217a6d41e7e487ddd40c911bbbd05186aa44
Sinthex91 <49324747+Sinthex91@users.noreply.github.com>
2020-03-01 22:04:16 +0100

    Optimisation
    Shorter code

--- js/events.js
+++ js/events.js
@@ -1,26 +1,17 @@
-// function onLoadEvent() {
-//   alert("Ich Lebe! HALLO WELT!");
-// }
-// window.onload = onLoadEvent;
-
 function nixBlur(eventObj) {
   let img = eventObj.target;
   img.style.filter = "blur(0px)";
-  setTimeout(neuesSpiel, 3000);
 }
-function neuesSpiel() {
-  let images = document.getElementsByTagName("img");
-  for (let i = 0; i < images.length; i++) {
-    images[i].style.filter = "blur(10px)";
-  }
+function reBlur(eventObj) {
+  let img = eventObj.target;
+  img.style.filter = "blur(8px)";
 }
 
 function init() {
   let images = document.getElementsByTagName("img");
   for (let i = 0; i < images.length; i++) {
-    images[i].onclick = nixBlur;
+    images[i].onmouseover = nixBlur;
+    images[i].onmouseout = reBlur;
   }
-  let neuStart = document.getElementById("neuStart");
-  neuStart.onclick = neuesSpiel;
 }
 window.onload = init;
